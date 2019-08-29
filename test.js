f7ef6cd7cfd2f948524e1c56be01d98f4f1d1167
ShaunLWM <shaunlimweimin@gmail.com>
2019-08-30 00:47:02 +0800

    First test experiment

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
 const googlecal = require("./"); // require("node-googlecal")
-console.log(googlecal({ title: "Hello World", location: "Google Seattle" }));
-console.log(googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase" }));
-console.log(googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase", dateStart: "20190808T170000Z", dateEnd: "20190808T173000Z" }));
-console.log(googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase", dateStart: "20190808T170000Z", dateEnd: "20190808" }));
+const assert = require("assert");
+
+describe("Google Calendar Tests", function () {
+    it("should return link with location only", function () {
+        let cal = googlecal({ title: "Hello World", location: "Google Seattle" });
+        assert.strictEqual(cal["error"], "");
+        assert.strictEqual(cal["url"], "https://calendar.google.com/calendar/r/eventedit?text=Hello%20World&location=Google%20Seattle");
+    });
+
+    it("should return link with location and description", function () {
+        let cal = googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase" });
+        assert.strictEqual(cal["error"], "");
+        assert.strictEqual(cal["url"], "https://calendar.google.com/calendar/r/eventedit?text=Hello%20World&location=Google%20Seattle&details=Join%20us%20for%20a%20wonderful%20evening%20about%20Firebase");
+    });
+
+    it("should return link with location, description & time", function () {
+        let cal = googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase", dateStart: "20190808T170000Z", dateEnd: "20190808T173000Z" });
+        assert.strictEqual(cal["error"], "");
+        assert.strictEqual(cal["url"], "https://calendar.google.com/calendar/r/eventedit?dates=20190808T170000Z%2F20190808T173000Z&text=Hello%20World&location=Google%20Seattle&details=Join%20us%20for%20a%20wonderful%20evening%20about%20Firebase");
+    });
+
+    it("should have time error message", function () {
+        let cal = googlecal({ title: "Hello World", location: "Google Seattle", description: "Join us for a wonderful evening about Firebase", dateStart: "2019-12-10", dateEnd: "2019-12-12" });
+        assert.strictEqual(cal["error"], "dateStart/dateEnd is in wrong format (Correct example: 20190808T170000Z)");
+        assert.strictEqual(cal["url"], "https://calendar.google.com/calendar/r/eventedit?text=Hello%20World&location=Google%20Seattle&details=Join%20us%20for%20a%20wonderful%20evening%20about%20Firebase");
+    });
+});
